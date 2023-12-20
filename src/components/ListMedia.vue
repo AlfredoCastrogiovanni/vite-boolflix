f1a9a9a99088f8b177bb77dfcd4a6580b317ae94
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-12-20 15:18:08 +0100

    create default search state

--- src/components/ListMedia.vue
+++ src/components/ListMedia.vue
@@ -19,9 +19,12 @@
 
 <template>
     <div class="container-fluid">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center" v-if="store.movieList.length > 0 || store.tvList.length > 0">
             <SingleMovie v-for="movie in store.movieList" :movie="movie"/>
             <SingleTv v-for="tv in store.tvList" :tv="tv"/>
+        </div>
+        <div class="row justify-content-center mt-5" v-else>
+            <span class="text-center mt-5 fs-2">Search Movies & Series there!</span>
         </div>
     </div>
 </template>
