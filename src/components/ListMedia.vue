e77bc884bfd84fd7f78b1e23436626dd06ddfff0
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-12-20 12:39:11 +0100

    create components and header

---
+++ src/components/ListMedia.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+    name: "ListMedia"
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SingleMedia.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+    name: "SingleMedia"
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
